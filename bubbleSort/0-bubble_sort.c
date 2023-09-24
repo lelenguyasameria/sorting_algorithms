da88cb75a046c8bc650513a3cba992a96b1eb11b
lelenguyasameria <somearia711@gmail.com>
2023-09-24 02:17:04 -0700

    bubblesort

---
+++ bubbleSort/0-bubble_sort.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+/**
+ * bubble_sort - Sorts an array integers in ascending order using Bubble sort.
+ * @array: The array to be sorted.
+ * @size: The size of the array.
+ *
+ * Description:
+ * This function sorts the given integer array in ascending order
+ * using the Bubble sort algorithm. It prints the array after each swap.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp;
+	int swapped;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = 0; /* Flag to check if any swaps were made during this pass */
+
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				/* Swap array[j] and array[j + 1] */
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+
+				swapped = 1; /* Set the flag to indicate a swap was made */
+
+				/* Print the array after each swap */
+				for (size_t k = 0; k < size; k++)
+				{
+					if (k == size - 1)
+						printf("%d\n", array[k]);
+					else
+						printf("%d, ", array[k]);
+				}
+			}
+		}
+
+		/* If no swaps were made during this pass, the array is already sorted */
+		if (swapped == 0)
+			break;
+	}
+}
+
---
+++ bubbleSort/README.md
@@ -0,0 +1,2 @@
+This project was undertaken by lelenguyasameria on the 21st of september as a requirement in the alx software engineering program
+the project involved the diferent types of algorithns focusing mainly in the bubble sort algorithm
