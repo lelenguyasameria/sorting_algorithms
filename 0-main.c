b7214846392ea2f70fcf8fddfd9e785a03feabe3
lelenguyasameria <somearia711@gmail.com>
2023-09-25 02:38:42 -0700

    0main

---
+++ 0-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main (void)
+{
+	int array [] = {15,45,88,63,10,47,86,64,76,6};
+	size_t n = sizeof (array) / sizeof (array[0]);
+	
+	ptint_array (array , n);
+	printf("\n");
+	bubble_sort (array , n);
+	printf("\n");
+	print_array (array , n );
+	
+	return (0);
+}
