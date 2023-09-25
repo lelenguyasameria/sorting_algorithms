96baab52422cd9347030e2a4f97e2676e8ce84e6
lelenguyasameria <somearia711@gmail.com>
2023-09-25 01:48:33 -0700

    sortbubble

--- sort.h
+++ sort.h
@@ -22,72 +22,23 @@ true
 /**
  * struct listint_s - singly linked list of integers
  * @n: Integer value stored in the node
+ * @prev:pointer to previus element
  * @next: Pointer to the next node
  *
  * Description: This structure defines a node for a singly linked list
  * of integers.
  */
-typedef struct listint_s {
-    int n;
-    struct listint_s *next;
+typedef struct listint_s
+{
+   const int n;
+   struct listint_s *prev;
+   struct listint_s *next;
 } listint_t;
 
-/**
- * print_array - Prints an array of integers
- * @array: The array to be printed
- * @size: The size of the array
- *
- * Description: This function prints the elements of an integer array.
- */
-void print_array(const int *array, size_t size);
+/*printing algorithm */
+void print_array (int arr[],int size);
+void print_list (struct Node * head);
 
-/**
- * print_list - Prints a singly linked list of integers
- * @head: Pointer to the head of the linked list
- *
- * Description: This function prints the elements of a singly linked list.
- */
-void print_list(const listint_t *head);
-
-/**
- * insertion_sort_list - Sorts a doubly linked list of integers in ascending orderWq
- 
-* @list: Pointer to a pointer to the head of the linked list
- *
- * Description: This function sorts a doubly linked list of integers in ascending order
- * using the insertion sort algorithm.
- */
-void insertion_sort_list(listint_t **list);
-
-/**
- * bubble_sort - Sorts an array of integers in ascending order using bubble sort
- * @array: The array to be sorted
- * @size: The size of the array
- *
- * Description: This function sorts an integer array in ascending order using the
- * bubble sort algorithm.
- */
-void bubble_sort(int *array, size_t size);
-
-/**
- * selection_sort - Sorts an array of integers in ascending order using selection sort
- * @array: The array to be sorted
- * @size: The size of the array
- *
- * Description: This function sorts an integer array in ascending order using the
- * selection sort algorithm.
- */
-void selection_sort(int *array, size_t size);
-
-/**
- * quick_sort - Sorts an array of integers in ascending order using quick sort
- * @array: The array to be sorted
- * @size: The size of the array
- *
- * Description: This function sorts an integer array in ascending order using the
- * quick sort algorithm.
- */
-void quick_sort(int *array, size_t size);
 /*sorting algorithms*/
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
