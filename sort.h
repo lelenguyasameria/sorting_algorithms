46cf54e5c4f055c7c83143d4c99e9aed80cbea4a
lelenguyasameria <somearia711@gmail.com>
2023-09-26 11:05:32 -0700

    my commit

--- sort.h
+++ sort.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* comparison direction macros for bitonic sort */
+/* Comparison direction macros for bitonic sort */
 #define UP 0
 #define DOWN 1
 
@@ -15,18 +15,16 @@
  */
 typedef enum bool
 {
-    false = 0,
-    true
+	false = 0,
+	true
 } bool;
 
 /**
- * struct listint_s - singly linked list of integers
- * @n: Integer value stored in the node
- * @prev: pointer to previous element
- * @next: Pointer to the next node
+ * struct listint_s - Doubly linked list node
  *
- * Description: This structure defines a node for a singly linked list
- * of integers.
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
  */
 typedef struct listint_s
 {
@@ -35,12 +33,11 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* printing algorithm */
-void print_array(int arr[], int size);
-void print_list(const listint_t *head);
-void insertion_sort_list(listint_t **list);
+/* Printing helper functions */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 
-/*sorting algorithms*/
+/* Sorting algoritms */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
@@ -53,6 +50,5 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-void sort_deck(deck_node_t **deck);
 
 #endif /* SORT_H */
